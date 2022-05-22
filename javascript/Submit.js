2fc20b8a16a707ae7bf76cd0ef2f40ffbe093bdf
cesar david moreno <cesardaviddev@gmail.com>
2022-05-22 11:25:44 +0200

    Mobile firts beggining of design and note

---
+++ javascript/Submit.js
@@ -0,0 +1,12 @@
+const sentBtn = document.getElementById('sent-btn');
+
+const onSubmit = () => {
+  sentBtn.style.color = 'red';
+  
+}
+
+sentBtn.addEventListener("submit", onSubmit);
+
+/* https://www.youtube.com/watch?v=B-ytMSuwbf8 */
+
+TODO /* https://www.youtube.com/watch?v=B-ytMSuwbf8 */
